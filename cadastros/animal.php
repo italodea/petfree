771d23b60dd81ee2d1ca87e23b8bc3dc3c47941f
italo de azevedo <italodeazevedo99@gmail.com>
2019-01-07 01:12:44 -0200

    Nova possibilidade de enviar as fotos como link ou arquivo.

--- cadastros/animal.php
+++ cadastros/animal.php
@@ -1,71 +1,75 @@
-<?php 
+<?php
 
-include '../etc/conexao.php';
+session_start();
 
-$nome = $_POST['nome'];
-$idade = $_POST['idade'];
-$caracteristicas = $_POST['caracteristicas'];
-$tipo_animal = $_POST['tipo'];
+function cadastrar_pet(){
+	include '../etc/conexao.php';
+	# estrutura básica para cadastrar as informações sobre o pet
+	$nome = $_POST['nome'];
+	$idade = $_POST['idade'];
+	$tipo_animal = $_POST['tipo'];
+	$caracteristicas = $_POST['caracteristicas'];
+	$query = "INSERT INTO animal(`nome`,`caracteristicas`,`idade`,`tipo`) VALUES('$nome','$caracteristicas','$idade','$tipo_animal');";
 
+	$run = mysqli_query($con,$query);
+}
 
-
-//arquivo
-$extensões_permitidas = ['jpeg','jpg','png'];
-$local_salvamento = "../fotos_animais/";
-
-$nome_arquivo = $_FILES['foto']['name'];
-$nome_temporario = $_FILES['foto']['tmp_name'];
-$tamanho = $_FILES['foto']['size'];
-
-
-
-$tmp = explode('.', $nome_arquivo);
-$extensao =  end($tmp);
-$barrinha = explode(" ", "\ ");
-
-
-/*
-
-Aviso: Desfaça o comentário da variavel $tmp1 que corresponde ao SO
-do servidor em que o software está sendo executado;
-
-*/
-
-$tmp1 = explode('/', $nome_temporario);//linux ou mac
-//$tmp1 = explode($barrinha[0], $nome_temporario);//windows
+function imagem_via_link(){
+	include '../etc/conexao.php';
+	$link = $_POST['imagem'];
+	$ultimo_animal = mysqli_fetch_array(mysqli_query($con,"select id from animal ORDER BY id DESC LIMIT 1;"));
+	$query = "INSERT INTO foto(`nomeArquivo`,`postador`,`animal`) VALUES('$link','".$_SESSION['id']."','".$ultimo_animal['id']."');";
+	$run = mysqli_query($con,$query);
 
 
 
-var_dump($tmp1);
-$novo_nome_arquivo = end($tmp1) . ".$extensao";
-$path_de_envio = $local_salvamento . basename($novo_nome_arquivo);
-echo "extensao:".$extensao."<br>";
-echo "nametmp:".$nome_temporario."<br>";
-echo "nome do arquivo:".$novo_nome_arquivo."<br>";
-echo "path:".$path_de_envio."<br>";
 
-/*try {
-	if ($tamanho > 2000000) {
-		echo "O arquivo tem de ter menos de 2MB";
-		//header("Location:../");
-	}else if(! in_array($extensao,$extensões_permitidas)) {
+}
+function imagem_loca(){
+	include '../etc/conexao.php';
+	$ultimo_animal = mysqli_fetch_array(mysqli_query($con,"select id from animal ORDER BY id DESC LIMIT 1;"));
+	
+	//Arquivos
+	$extensões_permitidas = ['jpeg','jpg','png'];
+	$local_salvamento = "../fotos_animais/";
+
+	$nome_arquivo = $_FILES['foto']['name'];
+	$nome_temporario = $_FILES['foto']['tmp_name'];
+	$tamanho = $_FILES['foto']['size'];
+	$tmp = explode('.', $nome_arquivo);
+	$extensao =  end($tmp);
+	$barrinha = explode(" ", "\ ");// linha auxiliar
+
+	if(! in_array($extensao,$extensões_permitidas)) {
 		echo "Extensão não suportada apenas arquivos JPEG ou PNG";
 		//header("Location:../");
 	}
-	session_start();
-	$fazerupload = move_uploaded_file($nome_temporario, $path_de_envio);
-	$query = "INSERT INTO animal(`nome`,`caracteristicas`,`idade`,`tipo`) VALUES('$nome','$caracteristicas','$idade','$tipo_animal');";
-	$run = mysqli_query($con,$query);
-	
-	$ultimo_animal = mysqli_fetch_array(mysqli_query($con,"select id from animal ORDER BY id DESC
-LIMIT 1;"));
+
+	/*
+
+	Aviso: Desfaça o comentário da variavel $tmp1 que corresponde ao SO
+	do servidor em que o software está sendo executado;
+
+	*/
+	//$tmp1 = explode('/', $nome_temporario);//linux ou mac
+	$tmp1 = explode($barrinha[0], $nome_temporario);//windows
+	$novo_nome_arquivo = end($tmp1) . ".$extensao";
+	$path_de_envio = $local_salvamento . basename($novo_nome_arquivo);
+
 	$query = "INSERT INTO foto(nomeArquivo,postador,animal) VALUES('$novo_nome_arquivo','".$_SESSION['id']."','".$ultimo_animal['id']."');";
 	$run = mysqli_query($con,$query);
-	echo "$query";
-	header("Location:../gerente.php");
-} catch (Exception $e) {
-	
-}
-*/
 
+}
+
+
+
+
+
+
+cadastrar_pet();
+
+//escolha se as imagem serão armazenadas só o link ou se serão armazenadas no servidor
+//imagem_local();
+imagem_via_link();
+header("Location:../gerente.php");
 ?>
